6c8b6903261655a486d87062cca511f314699306
Biraj Shrestha <86864374+Biraj1234@users.noreply.github.com>
2022-04-26 10:51:29 +0545

    data type of profile_picture is changed to save the image in base64 format

--- Server/app/Http/Controllers/FriendController.php
+++ Server/app/Http/Controllers/FriendController.php
@@ -5,31 +5,30 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FriendStoreRequest;
 use App\Http\Resources\FriendResource;
 use App\Models\Friend;
-use App\Models\User;
-use Faker\Provider\Image;
 use Illuminate\Http\Request;
 
 use function GuzzleHttp\Promise\all;
 
-class FriendController extends Controller{
+class FriendController extends Controller
+{
 
-    public function index(Request $request){
-       $friend = Friend::orderBy('created_at', 'DESC')->get();
-       if($request->search){
-        $friend = Friend::where('name','LIKE','%'.$request->search.'%')->get();
+    public function index(Request $request)
+    {
+        $friend = Friend::orderBy('created_at', 'DESC')->get();
+        if ($request->search) {
+            $friend = Friend::where('name', 'LIKE', '%' . $request->search . '%')->get();
         }
-       return FriendResource::collection($friend);
-
+        return FriendResource::collection($friend);
     }
 
     public function store(Request $request)
     {
 
         $file = base64_encode(file_get_contents($request->file('profile_name')->path()));
-        $request->request->add(['profile_picture'=>$file]);
+        $request->request->add(['profile_picture' => $file]);
 
 
-        $friend= Friend::create($request->all());
+        $friend = Friend::create($request->all());
         return response()->json($friend);
     }
 
@@ -41,12 +40,13 @@ class FriendController extends Controller{
 
     public function edit(Friend $friend)
     {
-  //
+        //
     }
 
 
-    public function update(FriendStoreRequest $request, Friend $friend){
-       $friend->update($request->validated());
+    public function update(FriendStoreRequest $request, Friend $friend)
+    {
+        $friend->update($request->validated());
     }
 
 
@@ -56,7 +56,8 @@ class FriendController extends Controller{
         return response()->json();
     }
 
-    public function home(Friend $friend){
+    public function home(Friend $friend)
+    {
         $friend = Friend::count();
         return response()->json($friend);
     }
